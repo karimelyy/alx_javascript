ab75ff06a18db1cef0e334ef25e813fc2925f665
karimelyy <elkarims30@gmail.com>
2024-01-30 12:10:42 +0100

    commit

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of strings
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a loop and console.log(...) to print the lines
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
