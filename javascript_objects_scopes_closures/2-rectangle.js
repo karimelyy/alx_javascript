b58e91a8b288a2654e31c2cb4fc2586e36472b3e
karimelyy <elkarims30@gmail.com>
2024-01-30 12:56:19 +0100

    commit

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,11 @@
 
 class Rectangle {
   constructor(w, h) {
-    // Check for positive integers or set default values
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      // Create an empty object for invalid inputs
+      // Set default values for invalid inputs
       this.width = undefined;
       this.height = undefined;
     }
