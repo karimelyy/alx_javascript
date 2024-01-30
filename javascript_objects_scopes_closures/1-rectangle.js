8165b48cfc4831efd0a626a22bb67ab8b21428ee
karimelyy <elkarims30@gmail.com>
2024-01-30 12:44:29 +0100

    commit

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,22 @@
 class Rectangle {
   constructor(w, h) {
-    // Set width and height to positive values
-    this.width = Math.max(1, w || 1);
-    this.height = Math.max(1, h || 1);
+    if (w !== undefined && h !== undefined) {
+      // Set width and height to positive values
+      this.width = Math.max(1, w);
+      this.height = Math.max(1, h);
+    } else if (w !== undefined) {
+      // Only width provided
+      this.width = Math.max(1, w);
+      this.height = 1;
+    } else if (h !== undefined) {
+      // Only height provided
+      this.width = 1;
+      this.height = Math.max(1, h);
+    } else {
+      // No arguments provided
+      this.width = 1;
+      this.height = 1;
+    }
   }
 }
 
