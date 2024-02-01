e6cd499613dc7e7aa2000cf6577917cb0c6c5cb8
karimelyy <elkarims30@gmail.com>
2024-02-01 12:37:36 +0100

    commit

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      return {};
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
